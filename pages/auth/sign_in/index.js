3b79712ec9714185d21f59fa71536186cf57b98c
Trisonweru <isaactrisonwaweru@gmail.com>
2021-09-08 15:46:02 +0300

    Third revison

--- pages/auth/sign_in/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SignUp from "../../../components/Signin/Signin";
-
-function index() {
-  return <SignUp />;
-}
-
-export default index;
--- pages/auth/sign_up/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SignUp from "../../../components/SignUp/SignUp";
-
-function index() {
-  return <SignUp />;
-}
-
-export default index;
