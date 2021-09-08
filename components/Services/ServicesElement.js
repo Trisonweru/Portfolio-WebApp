0e10d1f5be8c7ce7c52b39c849c902694b79976e
Trisonweru <isaactrisonwaweru@gmail.com>
2021-09-08 18:09:25 +0300

    Added padding to Services Element.js

--- components/Services/ServicesElement.js
+++ components/Services/ServicesElement.js
@@ -9,24 +9,6 @@ export const ServicesWrapper = styled.div`
   justify-content: center;
   margin-top: 20px;
 `;
-export const ServiceContainer = styled.div`
-  display: flex;
-  flex-flow: column;
-  width: 80%;
-  align-items: center;
-  justify-content: center;
-  height: 500px;
-  background: #fff;
-  border-radius: 10px;
-  margin-top: 20px;
-  box-shadow: 6px 6px 10px -1px rgba(0, 0, 0, 0.5),
-    -6px -6px 10px -1px rgba(255, 255, 255, 0);
-
-  @media screen and (max-width: 500px) {
-    height: 350px;
-    // width: 86%;
-  }
-`;
 
 export const ServiceHeadingWrapper = styled.div`
   display: flex;
