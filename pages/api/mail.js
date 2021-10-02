0dd03bf1c7818f4d29ee95711dbef029554017b4
Trisonweru <isaactrisonwaweru@gmail.com>
2021-10-02 12:58:00 +0300

    edited mail.js

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -6,7 +6,7 @@ async function sendEmail(req, res) {
     await sendgrid.send({
       to: "isaactrisonwaweru@gmail.com", // Your email where you'll receive emails
       from: "isaactrisonwaweru@gmail.com", // your website email address here
-      subject: `[Lead from website] - From ${req.body.project}`,
+      subject: `[Lead from website] - ${req.body.project}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
       <head>
